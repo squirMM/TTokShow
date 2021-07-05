ede801c3cbe2c9172040b2750b0ba9728bc17976
김지석 <plays0708@naver.com>
2021-07-05 22:31:03 +0900

    ssg import os 추가

--- 5_CRAWLING/2021_TM_SSG_Crawl.py
+++ 5_CRAWLING/2021_TM_SSG_Crawl.py
@@ -6,11 +6,13 @@ from bs4 import BeautifulSoup
 from pandas import DataFrame
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+import os
 
 
 #크롬드라이버 연결
-chrome_driver='C:/Users/USER/Desktop/홍머/한이음/데이터/chromedriver'
-driver = webdriver.Chrome(chrome_driver)
+chrome_driver=os.path.join('chromedriver')
+chrome_options = webdriver.ChromeOptions()
+driver = webdriver.Chrome(chrome_driver, options=chrome_options)
 data_list = []
 
 plusUrl = urllib.parse.quote_plus(input('검색어를 입력하시오 : '))
@@ -40,15 +42,16 @@ print("평점:", review_grade)
 
 
 def get_page_data(): 
-    users = driver.find_elements_by_css_selector('.user') # 사용자명 수집 
+    users = driver.find_elements_by_css_selector('.user.in') # 사용자명 수집 
     ratings = driver.find_elements_by_css_selector('.sp_cdtl.cdtl_cmt_per') # 평점 수집 
-    
+    review = driver.find_elements_by_css_selector('.desc_txt') #리뷰 수집
     # 사용자명수와 평점수가 같을 경우만 수집 
     if len(users) == len(ratings): 
         for index in range(len(users)): 
             data = {} 
             data['username'] = users[index].text 
-            data['rating'] = int(ratings[index].get_attribute('style')) 
+            data['rating'] = int(ratings[index].get_attribute('style')) / 20
+            data['review'] = review[index].text
             print(data) 
             data_list.append(data) 
 
@@ -59,12 +62,15 @@ get_page_data() # 버튼을 눌러서 페이지를 이동해 가면서 계속 �
 for page in range(1, total_page): 
     try: 
         print(str(page) + " page 수집 끝") 
-        button_index = page % 10 + 2 # 데이터 수집이 끝난 뒤 다음 페이지 버튼을 클릭 
+        button_index = page % 10 # 데이터 수집이 끝난 뒤 다음 페이지 버튼을 클릭 
+        print("한태희 개새끼1")
         driver.find_element_by_css_selector('.fn_GoCommentPage(\''+str(button_index)+'\')').click() 
+        print("한태희 개새끼2")
         time.sleep(5) #1 0page 수집이 끝나서 11로 넘어가기 위해서는 > 버튼을 눌러야 함. 
         if(page % 10 == 0): 
             driver.find_element_by_css_selector('.btn_next').click() 
-            time.sleep(5) # 해당 페이지 데이터 수집 
+            time.sleep(5) # 해당 페이지 데이터 수집
+         
         get_page_data()
     except: 
         print("수집 에러") 
