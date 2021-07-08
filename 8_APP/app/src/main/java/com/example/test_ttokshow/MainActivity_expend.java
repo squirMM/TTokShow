653b60f5396603b2564fa9b320f39c8c301c2ca0
sumin98214 <jsm6616@naver.com>
2021-07-09 02:51:05 +0900

    7.9 -확장 페이지 생성중 -에뮬레이터 하단바 삭제 해보는중 -메인페이지 키워드부터 리뷰까지 새로운 xml만들지 고려 중

--- 8_APP/app/src/main/java/com/example/test_ttokshow/MainActivity_expend.java
+++ 8_APP/app/src/main/java/com/example/test_ttokshow/MainActivity_expend.java
@@ -5,15 +5,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.WindowManager;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity_expend extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        setContentView(R.layout.activity_main);
-
+        setContentView(R.layout.activity_main_expend);
     }
-
 }
