ec38db15829dec1479097413190c52e1ef8f0cee
sumin98214 <jsm6616@naver.com>
2021-07-10 03:10:07 +0900

    에베베ㅔㅔㅔㅔㅔㅔㅔㅔㅔㅔㅔㅔㅔㅔㅔㅔㅔㅔㅔㅔㅔㅔㅔㅔㅔㅔㅔㅔㅔㅔㅔㅔㅔㅔㅔㅔㅔㅔㅔㅔㅔㅔㅔㅔㅔㅔㅔㅔㅔㅔ

--- 8_APP/app/src/main/java/com/example/test_ttokshow/MainActivity.java
+++ 8_APP/app/src/main/java/com/example/test_ttokshow/MainActivity.java
@@ -3,26 +3,30 @@ package com.example.test_ttokshow;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
-
+    int count=0;
     boolean visible =false;
-    Button open_bu;
+    ImageButton open_bu;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
-        open_bu=(Button)findViewById(R.id.open);
+        open_bu=(ImageButton)findViewById(R.id.open);
+        //open_bu.setOnClickListener(this::onClick) ;
     }
-    public void click_open(View v){
+    public void onClick(View view) {
+        count++;
         if(!visible)
         {
             LinearLayout inflatedLayout = (LinearLayout)findViewById(R.id.inflatedlayout);
