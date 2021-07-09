041984951b24a5a3e787a14b632d6eb13d701f39
sumin98214 <jsm6616@naver.com>
2021-07-10 01:19:52 +0900

    7.10 -inflationlayout 생성중 (정보 들어가야함) -image button으로 생성되야함

--- 8_APP/app/src/main/java/com/example/test_ttokshow/MainActivity.java
+++ 8_APP/app/src/main/java/com/example/test_ttokshow/MainActivity.java
@@ -2,18 +2,33 @@ package com.example.test_ttokshow;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
 
+    boolean visible =false;
+    Button open_bu;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
-
+        open_bu=(Button)findViewById(R.id.open);
     }
-
+    public void click_open(View v){
+        if(!visible)
+        {
+            LinearLayout inflatedLayout = (LinearLayout)findViewById(R.id.inflatedlayout);
+            LayoutInflater inflater =  (LayoutInflater)getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            inflater.inflate(R.layout.inflated_layout, inflatedLayout);
+            visible = true;
+        }
+    }
 }
